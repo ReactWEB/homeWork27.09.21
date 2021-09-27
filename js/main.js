281c31a79407287675fbab819ebbcc3f18ce0e65
ReactWEB <reactweb@ukr.net>
2021-09-27 16:48:23 +0300

    trying twice

--- js/main.js
+++ js/main.js
@@ -3,11 +3,9 @@
 
 const arr = [1, 8, -3, 4];
 
-const task1 = (_array) => {
+const task1 = (_array) => {      
   for(let i = 0; i < 100; i++){
-    if(arr.el !== 0){
-      arr.push(0);
-    }    
+    arr.push(0); 
   }
 }
 task1(0);
@@ -19,15 +17,11 @@ console.log(arr);
 const arr1 = [1, 8, -3, 4];
 
 const task2 = (_array) => {
-  for(let i = 0; i < 100; i++){
+  for(let i = 0; i < 96; i++){
     if(arr1.el !== 0){
       arr1.push(0);      
     }    
-  } 
-  arr1.pop(); 
-  arr1.pop();
-  arr1.pop();
-  arr1.pop();
+  }  
 }
 task2(0);
 
@@ -39,19 +33,9 @@ console.log(arr1);
 const arr2 = [1, 8, -3, 4];
 
 const task3 = (_array) => {
-  for(let i = 0; i <= 103; i++){
-    if(arr2.el !== 0){
-      arr2.push(0);          
-    }   
-  }  
-  let a = arr2.shift();
-  arr2[arr2.length -4] = a;
-  a = arr2.shift();
-  arr2[arr2.length -3] = a;
-  a = arr2.shift();
-  arr2[arr2.length -2] = a;
-  a = arr2.shift();
-  arr2[arr2.length -1] = a;
+  for(let i = 0; i < (203 - arr2.length); i++){    
+     arr2.unshift(0);
+    } 
 }
 task3(0);
 
@@ -64,19 +48,9 @@ console.log(arr2);
 const arr3 = [1, 8, -3, 4];
 
 const task4 = (_array) => {
-  for(let i = 0; i < 100; i++){
-    if(arr3.el !== 0){
-      arr3.push(0);          
-    }   
-  }  
-  let a = arr3.shift();
-  arr3[arr3.length -4] = a;
-  a = arr3.shift();
-  arr3[arr3.length -3] = a;
-  a = arr3.shift();
-  arr3[arr3.length -2] = a;
-  a = arr3.shift();
-  arr3[arr3.length -1] = a;
+  for(let i = 0; i < (196 - arr3.length); i++){    
+      arr3.unshift(0);          
+    } 
 }
 task4(0);
 
@@ -90,7 +64,7 @@ const arr4 = [1, 8, -3, 4];
 
 const task5 = (_array) => {
   let c = arr4.shift();
-  for(let i = 0; i < 26; i++){
+  for(let i = 0; i < (54 - arr4.length); i++){
     if(arr4.el !== 1){
       arr4.unshift(0);          
     }   
@@ -107,7 +81,7 @@ console.log(arr4);
 const arr5 = [1, 8, -3, 4];
 
 const task6 = (_array) => {
-  for(let i = 0; i < 19; i++){
+  for(let i = 0; i < 96 - arr5.length; i++){
     arr5.unshift(arr5[0], arr5[1], arr5[2], arr5[3]);
   }
 }
@@ -142,7 +116,7 @@ const task8 = (_array) => {
   for(let i = 0; i < arr7.length; i++){    
      if(arr7[i] > 0){
       sumPlus += 1;
-     } else if(arr7[i] < 0){
+     } else {
       sumMinus += 1;
      }
   } 
@@ -171,11 +145,12 @@ console.log(arr8);
 
 const arr9 = [1, 8, -3, 4];
 
-const task10 = (_array) => {       
-      arr9[4] = arr9[0] + 3;
-      arr9[5] = arr9[1] + 3;
-      arr9[6] = arr9[2] + 3;
-      arr9[7] = arr9[3] + 3;  
+const task10 = (_array) => {  
+       
+  for(let i = 0; i < 12 - arr9.length; i++){
+    let x = arr[i] + 3;       
+    arr9.push(x);    
+  }  
 }
 task10(0);
 console.log(arr9);
@@ -195,12 +170,13 @@ console.log(arr10);
 //12) получить значение последнего элемента массива, на это значение увеличить оставшиеся элементы. // [5, 12, 1]
 
 const arr11 = [1, 8, -3, 4];
-let g = arr11[arr11.length - 1];
+let g = arr11.pop();
+
 const task12 = (_array) => {  
   for(let i = 0; i < arr11.length; i++){
     arr11[i] = arr11[i] + g;    
   }
-  arr11[arr11.length - 1] /= 2;
+  arr11.push(g);
 }
 task12(0);
 console.log(arr11);
